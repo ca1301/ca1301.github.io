57626dd7c09ea75bb67d8ac646df538e6e670549
ca1301 <34408387+ca1301@users.noreply.github.com>
2021-06-07 17:06:17 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http');
 const port = process.env.PORT || 3000
+var fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World this updated automatically</h1>');
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
 });
 
 server.listen(port,() => {
